e45387c58e6890d257163c9fbd4016e6b83a4063
sim-sal <simone.saladino93@gmail.com>
2023-12-05 15:26:53 +0100

    first commit

---
+++ src/components/MyFormData.jsx
@@ -0,0 +1,11 @@
+import style from "../css/modules/MyFormData.module.css";
+
+
+export default function MyFormData() {
+
+    return (
+        <div className={style.my_container}>
+            <h1>INSERISCI I DATI DEL NUOVO POST</h1>
+        </div>
+    );
+}
